f82368afd4728cc7810fb97e2f5666fea193928d
J0e9u0 <joe.guo@oocl.com>
2018-11-28 17:46:49 +0800

    add Demo3

---
+++ Demo3/js/routes.js
@@ -0,0 +1,16 @@
+var routes = [
+  {
+    path: '/info',
+    url: './pages/info.html',
+    
+  },
+  {
+    path: '/index',
+    url: './index.html',
+    
+  },
+  {
+    path: '/about/',
+    url: './pages/about.html'
+  }
+]
