25d7d229989b28e720a940f84163157e931709f7
J0e9u0 <joe.guo@oocl.com>
2018-11-28 17:47:28 +0800

    add framework7 learn demo

---
+++ framework7/js/routes.js
@@ -0,0 +1,14 @@
+var routes = [
+  // Index page
+  {
+    path: '/info1',
+    url: 'info1.html',
+    name: 'info1',
+  },
+  // About page
+  {
+    path: '/about/',
+    url: './pages/about.html',
+    name: 'about',
+  }
+];
