7c429e66a6224f71e093c08fb7e8ba3833fbf639
sicmatr1x <sicmatr1x@outlook.com>
2018-12-09 21:28:31 +0800

    add vue test

---
+++ Vue/component/singleFileComponent/blog-post.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="blog-post">
+        <h3>{{ post.title }}</h3>
+        <div v-html="post.content"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "blog-post",
+        prop: {
+            post:{
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
